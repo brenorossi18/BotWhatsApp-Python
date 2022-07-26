69f58531e5e959889c7c7a4baf1f7bbeddfd226e
Breno Rossi <brenorossi18@gmail.com>
2022-07-26 12:31:31 -0300

    UpdateAutomacaoV2

--- aplicativo.py
+++ aplicativo.py
@@ -1,25 +1,61 @@
 # Instalar biblioteca
+# pip install pyodbc
 # pip install keyboard pywhatkit
 
 # importando bibliotecas
+import pyodbc
 import pywhatkit
 import keyboard
 import time
 from datetime import datetime
 
-# Definir quais contatos enviar mensagem
-contatos = ['+5511', '+5521']
-mensagem = 'Digite a sua mensagem'
+
+# Conexão com o banco de dados
 
 
-# Definir intervalo de envio
-while len(contatos) >= 1:
-    # enviar mensagem
-    pywhatkit.sendwhatmsg(
-        contatos[0], mensagem, datetime.now().hour, datetime.now().minute + 1)
-    # Deleta contato inicial para não repetir envio
-    del contatos[0]
-    # Tempo de espera
-    time.sleep(6)
-    # Fecha a aba
-    keyboard.press_and_release('ctrl + w')
+def conexao_bd():
+    server = 'server'
+    database = 'database'
+    username = 'username'
+    password = 'password'
+    driver = 'SQL Server'
+
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server +
+                          ';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
+
+    return cnxn
+
+
+conexao = conexao_bd()
+cursor = conexao.cursor()
+query_contato = cursor.execute('SELECT telefone  FROM [dbo].[contatos]')
+
+mensagem = """Olá,
+
+  Teste da automação.
+
+  Atenciosamente.
+
+  """
+
+contatos = []
+for linha in cursor.fetchall():
+    contatos.append(list(linha))
+
+    for contato in contatos:
+        while len(contato) >= 1:
+            # enviar mensagem
+            pywhatkit.sendwhatmsg(
+                contato[0], mensagem, datetime.now().hour, datetime.now().minute + 1)
+            # Deleta contato inicial para não repetir envio
+            del contato[0]
+            # Tempo de espera
+            time.sleep(6)
+            # Fecha a aba
+            keyboard.press_and_release('ctrl + w')
+
+        contato
+
+
+conexao.commit()
+conexao.close()
